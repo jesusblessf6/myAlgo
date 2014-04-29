b43e640a079330820d2ae3b5441229388389334f
yirun0701 <yirun0701@icloud.com>
2014-04-29 18:01:21 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+// Get Sign up page
+router.get('/signup', function(req, res) {
+    res.render('signup', {
+        title: '注册'
+    });
 });
 
 module.exports = router;
