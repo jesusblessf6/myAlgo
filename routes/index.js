d3ee5658d13df8fc0aad49583774fc1563dd6b35
yirun0701 <yirun0701@icloud.com>
2014-04-30 15:32:50 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,11 @@ router.get('/signin', function(req, res) {
     });
 });
 
+//goto the algorithm page
+router.get('/algos', function(req, res) {
+    res.render('algos', {
+        title: '我的算法'
+    });
+});
+
 module.exports = router;
